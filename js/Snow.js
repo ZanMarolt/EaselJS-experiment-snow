109f8c582900d03fdcf7b0251c25f6a1e657f0d4
Zan Marolt <marolt.zan@hotmail.com>
2016-05-03 18:02:42 +0200

    Feauture: Add particles to stage.

--- js/Snow.js
+++ js/Snow.js
@@ -2,14 +2,21 @@
  * Created by zanmarolt on 5/3/16.
  */
 
-var Snow = function(){
+var Snow = function(container){
+
+    this.container = container;
+
+    this._draw();
 
 };
 
-Snow.prototype.draw = function(){
+Snow.prototype._draw = function(){
 
-    for(){
+    this.particle = new createjs.Shape();
+    this.particle.graphics.beginFill('#000000');
+    this.particle.graphics.drawRect(0,0,50,50);
+    this.particle.graphics.endFill();
 
-    }
+    this.container.addChild(this.particle);
 
 };
--- js/app.js
+++ js/app.js
@@ -1,29 +1,46 @@
 var app = {
 
+    particleArray: [],
     stage:null,
+
+    NUM_PARTICLES: 100,
 
     init:function(){
 
         app.setupStage();
+        app.setupTicker();
+        app.createParticles();
 
     },
     setupStage:function(){
+
         var $canvas = $('#snow-canvas');
 
         // To get out the first in the array (element)
         app.stage = new createjs.Stage($canvas[0]);
 
-        var square = new createjs.Shape();
-        square.graphics.beginFill('#444444');
-        square.graphics.drawRect(0,0,50,50);
-        square.graphics.endFill();
+    },
+    setupTicker:function(){
 
-        app.stage.addChild(square);
+        if(!app.stage) throw new Error('Hold your horses, buddy. Stage is not yet defined.');
 
         createjs.Ticker.setFPS(60);
         createjs.Ticker.addEventListener('tick', app.stage);
 
-        TweenLite.to(square, 2, { x:100 })
+    },
+    createParticles:function(){
+
+        while(app.NUM_PARTICLES--){
+
+            var snow = new Snow(app.stage);
+
+            snow.particle.x = Math.random()*1000;
+            snow.particle.y = Math.random()*1000;
+
+            app.particleArray.push(snow);
+
+        }
+
     }
 };
 
