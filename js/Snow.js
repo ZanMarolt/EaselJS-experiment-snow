40a6f293277c3bf47d0cec412d30bd50e0fd781d
Zan Marolt <marolt.zan@hotmail.com>
2016-05-03 18:19:23 +0200

    Feature: Added the falling of the snowflakes.

--- js/Snow.js
+++ js/Snow.js
@@ -8,15 +8,27 @@ var Snow = function(container){
 
     this._draw();
 
+    createjs.Ticker.addEventListener('tick', this._onAnimation.bind(this));
+
 };
 
 Snow.prototype._draw = function(){
 
-    this.particle = new createjs.Shape();
-    this.particle.graphics.beginFill('#000000');
-    this.particle.graphics.drawRect(0,0,50,50);
-    this.particle.graphics.endFill();
+    this.particle = new createjs.Container();
+
+    this.shape = new createjs.Shape();
+    this.shape.graphics.beginFill('#000000');
+    this.shape.graphics.drawRect(0,0,50,50);
+    this.shape.graphics.endFill();
+
+    this.particle.addChild(this.shape);
 
     this.container.addChild(this.particle);
+
+};
+
+Snow.prototype._onAnimation = function(){
+
+    this.particle.y += 1;
 
 };
