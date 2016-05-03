a38889ffbda85867e2c35668339620bd238ae074
Zan Marolt <marolt.zan@hotmail.com>
2016-05-03 16:47:46 +0200

    Feature: Basic structure setup.

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+/**
+ * Created by zanmarolt on 5/3/16.
+ */
+
+var gulp = require('gulp');
+var serve = require('gulp-serve');
+
+gulp.task('serve', serve('./'));
---
+++ js/Snow.js
@@ -0,0 +1,3 @@
+/**
+ * Created by zanmarolt on 5/3/16.
+ */
