8cbaf7a2b3d6a09a30e80f5e737c08714078c3a1
Zan Marolt <marolt.zan@hotmail.com>
2016-05-03 17:04:38 +0200

    Feature: Drawing basic square onto the canvas

--- js/Snow.js
+++ js/Snow.js
@@ -1,3 +1,15 @@
 /**
  * Created by zanmarolt on 5/3/16.
  */
+
+var Snow = function(){
+
+};
+
+Snow.prototype.draw = function(){
+
+    for(){
+
+    }
+
+};
--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log('ready');
+var app = {
+
+    stage:null,
+
+    init:function(){
+
+        app.setupStage();
+
+    },
+    setupStage:function(){
+        var $canvas = $('#snow-canvas');
+
+        // To get out the first in the array (element)
+        app.stage = new createjs.Stage($canvas[0]);
+
+        var square = new createjs.Shape();
+        square.graphics.beginFill('#444444');
+        square.graphics.drawRect(0,0,50,50);
+        square.graphics.endFill();
+
+        app.stage.addChild(square);
+
+        app.stage.update();
+    }
+};
+
+app.init();
